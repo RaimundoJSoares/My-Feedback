2eb0495d784064e25e651753ad2c988f49a05510
RaimundoJSoares <raianimes1000@gmail.com>
2022-06-25 09:51:40 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from './components/Header'
+import './globalStyles.css'
+
+
+function App() {
+  
+
+  return (
+   <>
+    < Header />
+
+    <h1>Hello universe</h1>
+    </>
+  )
+}
+
+export default App
